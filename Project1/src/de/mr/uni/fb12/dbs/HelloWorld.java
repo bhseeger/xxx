fa35b7a97605bc66121678c8d916b1559b76ad8c
bhseeger <bhseeger@137.248.121.102>
2017-08-22 13:34:13 +0200

    Das ist ein Test.

---
+++ Project1/src/de/mr/uni/fb12/dbs/HelloWorld.java
@@ -0,0 +1,17 @@
+package de.mr.uni.fb12.dbs;
+
+/**
+ * @author Bernhard Seeger
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args Arguments from the Console
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Earth! " + new Second().getI());
+		// System.err.println("Hello Mars!");
+	}
+
+}
---
+++ Project1/src/de/mr/uni/fb12/dbs/Second.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package de.mr.uni.fb12.dbs;
+
+/**
+ * @author bhseeger
+ *
+ */
+public class Second {
+	private int i;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+	
+
+}

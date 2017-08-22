ed57291855f1c518a9c6a72851a2cac1696f98d8
bhseeger <bhseeger@137.248.121.102>
2017-08-22 13:36:54 +0200

    Dies ist Commit2.

--- Project1/src/de/mr/uni/fb12/dbs/HelloWorld.java
+++ Project1/src/de/mr/uni/fb12/dbs/HelloWorld.java
@@ -11,7 +11,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello Earth! " + new Second().getI());
-		// System.err.println("Hello Mars!");
+		System.out.println("Hello Mars! ");
 	}
 
 }
